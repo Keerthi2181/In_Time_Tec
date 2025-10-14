2a837384694ecebd667c6883419d27e085c8b863
Keerthi2181 <keerthig1218@gmail.com>
2025-10-14 11:50:29 +0530

    Update studentPerformanceAnalyzer.c
    Input validations included
    1.Roll number validation: must be positive.
    2.Marks validation: can only be between 0 and 100.
    3.Name validation: can contain only alphabets and spaces.
    4.Number of students: must be positive.

--- studentPerformanceAnalyzer.c
+++ studentPerformanceAnalyzer.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
 struct Student{
     int rollno;
     char name[20];
@@ -6,7 +9,14 @@ struct Student{
     int m2;
     int m3;
 };
-
+int checkValidName(char *name) {
+    if (!name || strlen(name) == 0) return 0;
+    for (int i = 0; name[i]; i++) {
+        if (!isalpha(name[i]) && name[i] != ' ')
+            return 0;
+    }
+    return 1;
+}
 int calculatetotal(int m1,int m2,int m3){
     int total=m1+m2+m3;
     return total;
@@ -37,35 +47,65 @@ void showperformance(char grade){
     printf("\n");
     
 }
-void printrollno(struct Student s[],int i,int n) {
-    if(i==n) return;
+void printrollno(struct Student s[],int i,int numofstudents) {
+    if(i==numofstudents) return;
     printf("%d ", s[i].rollno);
-    printrollno(s,i+1,n);
+    printrollno(s,i+1,numofstudents);
 }
 
 int main()
 {
-    int n;
+    int numofstudents;
     
     printf("Enter number of Students:");
-    scanf("%d",&n);
-    struct Student s[n];
-    for(int i=0;i<n;i++){
+    scanf("%d",&numofstudents);
+    while (numofstudents <= 0) {
+        printf("Invalid number! Enter again: ");
+        scanf("%d", &numofstudents);
+    }
+
+    struct Student s[numofstudents];
+    for(int i=0;i<numofstudents;i++){
         printf("Enter details of student%d:\n",i+1);
-        printf("Enter rollno:");
-        scanf("%d",&s[i].rollno);
-        printf("Enter name:");
-        scanf("%s",s[i].name);
-        printf("Enter marks1:");
-        scanf("%d",&s[i].m1);
-        printf("Enter marks2:");
-        scanf("%d",&s[i].m2);
-        printf("Enter marks3:");
-        scanf("%d",&s[i].m3);
+        printf("Enter rollno: ");
+        scanf("%d", &s[i].rollno);
+        while (s[i].rollno <= 0) {
+            printf("Invalid!Roll number can't be negative.Enter again: ");
+            scanf("%d", &s[i].rollno);
+        }
+        while (getchar() != '\n');
+        printf("Enter name: ");
+        scanf(" %[^\n]", s[i].name);
+        while (!checkValidName(s[i].name)) {
+            printf("Invalid! Only alphabets and spaces allowed. Enter again: ");
+            scanf(" %[^\n]", s[i].name);
+        }
+
+        printf("Enter marks1: ");
+        scanf("%d", &s[i].m1);
+        while (s[i].m1 < 0 || s[i].m1 > 100) {
+            printf("Invalid! Marks should be between 0 and 100. Enter again: ");
+            scanf("%d", &s[i].m1);  
+        }
+        
+        printf("Enter marks2: ");
+        scanf("%d", &s[i].m2);
+        while (s[i].m2 < 0 || s[i].m2 > 100) {
+            printf("Invalid! Marks should be between 0 and 100. Enter again: ");
+            scanf("%d", &s[i].m2);
+        }
+        
+        printf("Enter marks3: ");
+        scanf("%d", &s[i].m3);
+        while (s[i].m3 < 0 || s[i].m3 > 100) {
+            printf("Invalid! Marks should be between 0 and 100. Enter again: ");
+            scanf("%d", &s[i].m3);
+        }
+
         printf("\n\n");
     }
     
-    for(int i=0;i<n;i++){
+    for(int i=0;i<numofstudents;i++){
         int total=calculatetotal(s[i].m1,s[i].m2,s[i].m3);
         float avg=calculateavg(total);
         char grade=calculategrade(avg);
@@ -75,13 +115,16 @@ int main()
         printf("Total:%d\n",total);
         printf("Average:%.2f\n",avg);
         printf("Grade:%c\n",grade);
-        if(avg<35) continue;
+        if(avg<35){
+            printf("\n");
+            continue;
+        } 
         showperformance(grade);
         printf("\n");
         
     }
     printf("\nList of Rollno:");
-    printrollno(s,0,n);
+    printrollno(s,0,numofstudents);
     printf("\n");
     
 
